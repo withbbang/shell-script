254a6343622075413f9983490e5544992b61d6b2
Bread <withbbang@gmail.com>
2021-11-18 23:08:28 +0900

    second commit

---
+++ export_test.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#부모 스크립트 파일에서 불러온 후 자식은 부모 스크립트 파일에서 export 변수 출력
+echo ${hello_world}
---
+++ hello_2.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+#변수 할당할 때 = 양 옆으로 띄어쓰기 하면 안된다.
+#전역 변수 지정
+string="hello world"
+echo ${string}
+
+# 지역 변수 테스트 함수
+string_test() {
+	#전역 변수와 동일하게 사용함. 만약 local을 뺀다면 전역 변수에 덮어 씌워지게 됨
+	local string="local val"
+	echo ${string}
+}
+
+#지역 변수 테스트 함수 호출
+string_test
+#지역 변수 테스트 함수에서 동일한 변수 명을 사용했지만 값이 변경되지 않음
+echo ${string}
+
+#환경 변수 선언
+export hello_world="hello world..."
+./export_test.sh
