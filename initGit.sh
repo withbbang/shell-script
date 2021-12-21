991cedcfd391bfc24091239e9557d8d6f994307c
Bread-Kim <54889626+withbbang@users.noreply.github.com>
2021-12-21 23:44:10 +0900

    Update initGit.sh

--- initGit.sh
+++ initGit.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
+# 깃 프로젝트 폴더에서 실행할 것
+# 선행조건: git 설치, sh 실행 가능 환경
+# 파라미터: 깃 주소 (ex: $initGit.sh <git repos url>)
 
 echo "Git Initialization" >> README.md
 sleep 1
 git init &&
-sleep 1
 git add README.md &&
-sleep 1
 git commit -m "first commit" &&
-sleep 1
 git remote add origin $1 &&
-sleep 1
 git push origin master
