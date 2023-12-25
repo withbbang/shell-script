a78bded9a3fc50b2943cdf62532901cdc05594b7
상남자 <54889626+withbbang@users.noreply.github.com>
2023-12-25 23:57:12 +0900

    hello_2.sh 업데이트

--- hello_2.sh
+++ hello_2.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
-#변수 할당할 때 = 양 옆으로 띄어쓰기 하면 안된다.
-#전역 변수 지정
+# 변수 할당할 때 = 양 옆으로 띄어쓰기 하면 안된다.
+# 전역 변수 지정
 string="hello world"
 echo ${string}
 
@@ -12,12 +12,12 @@ string_test() {
 	echo ${string}
 }
 
-#지역 변수 테스트 함수 호출
+# 지역 변수 테스트 함수 호출
 string_test
-#지역 변수 테스트 함수에서 동일한 변수 명을 사용했지만 값이 변경되지 않음
+# 지역 변수 테스트 함수에서 동일한 변수 명을 사용했지만 값이 변경되지 않음
 echo ${string}
 
-#환경 변수 선언
+# 환경 변수 선언
 export hello_world="hello world..."
-#외부 스크립트 파일 호출
+# 외부 스크립트 파일 호출
 ./export_test.sh
